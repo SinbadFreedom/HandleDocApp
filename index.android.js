f4913ea9b0f656ea4675f2e98cdd10a06b61be6f
SinbadFreedom <151229152@qq.com>
2017-06-09 00:18:46 +0800

    init HandlebarsDocApp

---
+++ index.android.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    WebView,
+    StatusBar,
+    StyleSheet,
+    View,
+    BackHandler
+} from 'react-native';
+
+const styles = StyleSheet.create({
+                                     container: {
+                                         flex: 1,
+                                         backgroundColor: '#1FB9FF',
+                                         borderWidth: 0
+                                     },
+                                 });
+
+const WEB_VIEW_REF = "webView";
+
+class HandleBarsDocApp extends Component {
+
+    componentDidMount() {
+        BackHandler.addEventListener('hardwareBackPress', this.backHandler);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener('hardwareBackPress', this.backHandler);
+    }
+
+    backHandler = () => {
+        this.refs[WEB_VIEW_REF].goBack();
+        return true;
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true}></StatusBar>
+                <WebView
+                    ref={WEB_VIEW_REF}
+                    source={ {uri : 'http://handlebars.cn/'}}
+                />
+            </View>
+        );
+    }
+}
+
+AppRegistry.registerComponent('HandleBarsDocApp', () => HandleBarsDocApp);
